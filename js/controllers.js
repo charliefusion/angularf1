48cbaf0a5b4aed89d8e3e95b138cf48d8d928109
Charlie Thomason <charlie.thomason@razorfish.com>
2017-02-14 14:16:28 -0600

    style & formatting fixes

--- js/controllers.js
+++ js/controllers.js
@@ -24,10 +24,14 @@ app.controller('driverController', function($scope, $routeParams, $rootScope, er
 	$scope.id = $routeParams.id;
 	$scope.races = [];
 	$scope.driver = null;
+	if(!$rootScope.year) {
+		$rootScope.year = '2014';
+	}
 	ergastAPIservice.getDriverDetails($scope.id, $rootScope.year).success(function(response) {
 		$scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0];
 	});
 	ergastAPIservice.getDriverRaces($scope.id, $rootScope.year).success(function(response) {
+		document.getElementById('loading-gif').style.display = 'none';
 		$scope.races = response.MRData.RaceTable.Races;
 	});
 });
